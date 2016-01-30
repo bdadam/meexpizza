17b35c42213fb55464b503e843de5357e384f56d
bdadam <bdadam@gmail.com>
2016-01-30 20:37:48 +0100

    menu as yaml file

--- data-test.js
+++ data-test.js
@@ -1,9 +1,28 @@
 const fs   = require('fs');
 const yaml = require('js-yaml');
 
-const doc = yaml.safeLoad(fs.readFileSync('menu.yml', 'utf8'));
+const getslug = text => require('speakingurl')(text, { lang: 'hu' });
+
+const fileContent = fs.readFileSync('menu.yml', 'utf8');
+const doc = yaml.safeLoad(fileContent);
+
+const version = fileContent.match(/version\:\s([\S]+)/)[1];
 const categories = Object.keys(doc);
 
-console.log(categories);
+
+console.log(version);
+
+const convert = () => {
+
+    return {
+
+        categories
+
+    };
+
+
+    return doc;
+};
+
 
 // console.log(require('util').inspect(doc, { depth: null }));
--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,6 @@ module.exports = {
 		]
 	},
 	externals: {
-        // require("jquery") is external and available
-        //  on the global var jQuery
         "jquery": "jQuery"
     },
 	resolve: {
@@ -29,7 +27,7 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
-		new webpack.optimize.DedupePlugin(),
+		// new webpack.optimize.DedupePlugin(),
 		// new webpack.optimize.OccurrenceOrderPlugin(),
 		// new webpack.optimize.UglifyJsPlugin()
 	]
