1378422956118029eae73307e0ac466f03e8ca9b
bdadam <me@bdadam.com>
2016-09-12 16:26:29 +0200

    creating detail pages WIP

--- gulptasks/html.js
+++ gulptasks/html.js
@@ -4,6 +4,7 @@ const path = require('path');
 const YAML = require('yamljs');
 const menu = YAML.load('data/menu.yaml');
 const getSlug = text => require('speakingurl')(text, { lang: 'hu' });
+const mkdirp = require('mkdirp');
 
 module.exports = (gulp) => {
     gulp.task('html', () => {
@@ -18,12 +19,18 @@ module.exports = (gulp) => {
 
         fs.writeFileSync('dist/index.html', html);
 
-        Object.keys(menu['Étlap']).forEach(category => {
-            const urlCategory = getSlug(category);
-            Object.keys(menu['Étlap'][category]).forEach(dish => {
-                console.log(`${getSlug(category)}/${getSlug(dish)}/index.html`, category, dish);
-            });
-        });
+        // const generateDetailPageFor = (directory, category, dish, data) => {
+        //     mkdirp.sync(directory);
+        //
+        // };
+        //
+        // Object.keys(menu['Étlap']).forEach(category => {
+        //     const urlCategory = getSlug(category);
+        //     Object.keys(menu['Étlap'][category]).forEach(dish => {
+        //         const directory = `dist/${getSlug(category)}/${getSlug(dish)}`;
+        //         generateDetailPageFor(directory, category, dish, menu['Étlap'][category][dish]);
+        //     });
+        // });
     });
 
     gulp.task('html:watch', () => {
